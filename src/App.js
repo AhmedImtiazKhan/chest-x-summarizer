73ca7041d159c729bc25457d2e7361111a4d8846
Ahmed imtiaz <ahmed.imtiaz2000@gmail.com>
2023-05-22 14:33:33 +0500

    final classfification with build plus1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MenuBar from "./MenuBar";
 import Body from "./Body";
 import UploadPage from "./pages/Upload Image/UploadPage";
